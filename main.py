6987bd51a42972781b52d7762c044041e9c5e872
professor <professor@SN-407004.edu.fiesc.com.br>
2026-05-07 21:55:14 -0300

    criando jogo

---
+++ main.py
@@ -0,0 +1,16 @@
+print(" este eo jogo kingdom of shadows")
+print("ola grande cavalheiro")
+print("voce ira batalhar com o seu primeiro inimigo")
+print("voce estara com personagem draven ele e um cavalheiro ele usa uma espada como sua arma e ele tem 110 de hp ")
+print(" voce atacara primeiro por que voce é mais rapido do que seu inimigo")
+print("draven tem tres opçoes de ataque escolha")
+print("escolha 1 se voce quer usar corte 2 se voce quer usar multiplos cortes digite 3 se voce quer usar corte sagrado ")
+print("1 = corte tira 15 de hp")
+print("2 = multiplos cortes tira 27 de hp")
+print("3 = corte sagrado tira 35 de hp")
+ataque =  input("digite 1,2,3 para que ataque voce utilizara ")
+
+if  1: 
+    print(" voce usou corte voce tirou 15 de hp")
+elif 2:
+    print("voce usou multiplos cortes voce tirou 27 de hp")
