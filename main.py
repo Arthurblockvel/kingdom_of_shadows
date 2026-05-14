6b3ae26b20ee474e0d7c3f9031960529544dc12d
professor <professor@SN-407004.edu.fiesc.com.br>
2026-05-13 21:47:52 -0300

    fazendo jogo

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-print(" este eo jogo kingdom of shadows")
+import random
+print(" esse eo jogo kingdom of shadows")
+
 print("ola grande cavalheiro")
 print("voce ira batalhar com o seu primeiro inimigo")
 print("voce estara com personagem draven ele e um cavalheiro ele usa uma espada como sua arma e ele tem 110 de hp ")
+print("voce estara contra o sombra ele tem 60 de hp ")
+print (" ataques do sombra 1 soco 2 aranhar 3 assustar ")
+print("1 soco tira 10 de hp")
+print(" aranhar tira 18 de hp")
+print("assustar causa um debuff de 28% de dano")
 print(" voce atacara primeiro por que voce é mais rapido do que seu inimigo")
 print("draven tem tres opçoes de ataque escolha")
 print("escolha 1 se voce quer usar corte 2 se voce quer usar multiplos cortes digite 3 se voce quer usar corte sagrado ")
 print("1 = corte tira 15 de hp")
-print("2 = multiplos cortes tira 27 de hp")
-print("3 = corte sagrado tira 35 de hp")
-ataque =  input("digite 1,2,3 para que ataque voce utilizara ")
-
-if  1: 
+print("2 = multiplos cortes tira 24 de hp")
+print("3 = corte sagrado tira 35 de hp e deixa o inimigo sangrando")
+ataque =  input("digite 1,2,3 para qual ataque voce utilizara ")
+print()
+if ataque =="1":
     print(" voce usou corte voce tirou 15 de hp")
-elif 2:
-    print("voce usou multiplos cortes voce tirou 27 de hp")
+elif ataque =="2":
+    print("voce usou multiplos cortes voce tirou 24 de hp")
+elif ataque == "3":
+    print("voce usou corte sagrado voce tirou 35 de hp e deixou seu inimigo sangrando")
+print()
+print("agora a sombra ira atacar")
+print("sombra usou aranhar tirou 18 de hp")
